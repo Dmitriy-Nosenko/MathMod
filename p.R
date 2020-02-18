38deee5fa3bb979f5bd21de2ad2fdcff5fa74003
Dmitry-Nosenko <sakon6493@gmail.com>
2020-02-18 23:47:46 +0300

    Выполнение задания 1-3

--- p.R
+++ p.R
@@ -1,1 +1,19 @@
 h=1
+#Задание 1
+
+iris
+a=mean(iris$Sepal.Length);a
+b=mean(iris$Sepal.Width);b
+c=mean(iris$Petal.Length);c
+d=mean(iris$Petal.Width);d
+list(a,b,c,d)
+
+#Задание 2
+
+iris
+z=apply(iris[1:150,-5],1,mean);z
+
+#Задание 3
+
+DNA=sample(c("A","T","G","C"),size=1000,replace=TRUE);DNA
+dna_at=summary(factor(DNA))/length(DNA);dna_at
